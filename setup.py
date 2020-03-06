2d46464401eaf79b4ffc7edcddce9c868998d015
ssimonc <33287543+ssimonc@users.noreply.github.com>
2020-03-06 15:15:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(name='atomPython',
       version='1.0',
-      install_requires=['numpy', 'pandas', 'matplotlib'],  # And any other dependencies needed
-      packages = ["atomPython"]
+      packages = ["atomPython"],
+      install_requires=['numpy', 'pandas', 'matplotlib']  # And any other dependencies needed
+      
 
 )
+
