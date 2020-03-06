a5f9d5a364943d47368126e162bb30aa4a2fcec8
ssimonc <33287543+ssimonc@users.noreply.github.com>
2020-03-06 13:42:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='atomPython',
+      version='1.0',
+      install_requires=['numpy', 'pandas', 'matplotlib', 'copy']  # And any other dependencies needed
+)
