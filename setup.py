bb3fa0f8f7d6ce6341b95d83eb2bd4b35b6d6459
ssimonc <33287543+ssimonc@users.noreply.github.com>
2020-03-06 14:51:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(name='atomPython',
       version='1.0',
       install_requires=['numpy', 'pandas', 'matplotlib'],  # And any other dependencies needed
       packages = ["atomPython"],
-      package_dir= {'': ''},
       package_data={  # we need only the files below:
-        "instapy": [
+        "atomPython": [
             "atom.py",
              "binary_heap.py",
              "data_processing.py",
