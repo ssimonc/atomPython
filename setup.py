df63ac67524a92747f368114bdbbbcb3edf22cbf
ssimonc <33287543+ssimonc@users.noreply.github.com>
2020-03-06 14:46:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='atomPython',
       version='1.0',
       install_requires=['numpy', 'pandas', 'matplotlib'],  # And any other dependencies needed
+      packages = ["atomPython"]
       package_data={  # we need only the files below:
         "instapy": [
             "atom.py",
